44a67244379819d73d81b6025169c1c5636d53f6
Thomas Robinson <git@tomr.me>
2023-12-22 23:30:03 +0000

    use HEAD req, not GET for checking status (also single to double quotes)

--- static/js/404.js
+++ static/js/404.js
@@ -1,16 +1,16 @@
 const newURL = `https://github-pages.thomasr.me${location.pathname}`;
 
-if (location.pathname !== '/404.html') {
+if (location.pathname !== "/404.html") {
     const reqToCheck = new XMLHttpRequest();
-    reqToCheck.open('GET', newURL, true);
+    reqToCheck.open("HEAD", newURL, true);
     reqToCheck.onreadystatechange = function() {
         if (reqToCheck.readyState === 4) {
             if (reqToCheck.status === 404) {
                 // it doesn't exist
             } else {
-                document.title = 'This page has moved!';
-                document.getElementById('page-title').innerHTML = `This page has moved!`;
-                document.getElementById('page-description').innerHTML = `You should be redirected shortly.`;
+                document.title = "This page has moved!";
+                document.getElementById("page-title").innerHTML = "This page has moved!";
+                document.getElementById("page-description").innerHTML = "You should be redirected shortly.";
                 window.location = newURL;
             }
         }
